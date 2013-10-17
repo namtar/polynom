5938fe2ff0fcbb993c3dc84b3ec56b986403bdd3
namtar <matthias.drummer@gmail.com>
2013-10-17 10:25:46 +0200

    initial checkin of the polynom project.

---
+++ src/main/java/de/htw/berlin/student/polynom/Main.java
@@ -0,0 +1,15 @@
+package de.htw.berlin.student.polynom;
+
+/**
+ * Main class for the polynom program.
+ * 
+ * @author Matthias Drummer
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
