42ee4a61a924f75846b0ae65ec932dc25b15ec1d
namtar <matthias.drummer@gmail.com>
2013-11-02 12:53:17 +0100

    added plugins for jar building to the pom.xml

--- src/main/java/de/htw/berlin/student/polynom/controller/PolynomCalculatorController.java
+++ src/main/java/de/htw/berlin/student/polynom/controller/PolynomCalculatorController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
-import de.htw.berlin.student.polynom.helper.PolynomParser;
 import de.htw.berlin.student.polynom.io.ConsoleCommunicator;
 import de.htw.berlin.student.polynom.model.Polynom;
 
